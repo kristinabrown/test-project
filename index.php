4ecfbc05a6658fa6c00c2c5ab324f0145b573a0e
kristinabrown <kristina.frey.frey@gmail.com>
2025-03-27 18:31:35 +0100

    Return only acceptable fields

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ $app = require "./core/app.php";
 
 // Get all users from DB, eager load all fields using '*'
 $users = User::find($app->db,'*');
-
 // Render view 'views/index.php' and pass users variable there
+
 $app->renderView('index', array(
 	'users' => $users
 ));
--- models/user.php
+++ models/user.php
@@ -4,24 +4,24 @@
  * User model
  */
 class User extends BaseModel{
-	
+
 	// Define neccessary constansts so we know from which table to load data
 	const tableName = 'users';
 	// ClassName constant is important for find and findOne static functions to work
 	const className = 'User';
-	
+	const acceptableFields = ['id','name', 'email', 'city', 'created_at'];
 	// Create getter functions
-	
+
 	public function getName() {
 		return $this->getField('name');
 	}
-	
+
 	public function getEmail() {
 		return $this->getField('email');
 	}
-	
+
 	public function getCity() {
 		return $this->getField('city');
 	}
-	
+
 }
