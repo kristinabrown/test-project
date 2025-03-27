99ef1fd03adaabab374cb4875f0aab72ae89e046
kristinabrown <kristina.frey.frey@gmail.com>
2025-03-27 18:32:52 +0100

    Use Tailwind the quick way

--- views/layout.php
+++ views/layout.php
@@ -2,26 +2,27 @@
 <html>
   <head>
   	<meta charset="UTF-8">
-  	
+
     <title>PHP Test Application</title>
-    
+
 	<link href="favicon.ico" type="image/x-icon" rel="icon" />
-	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />	
-    
+	<link href="favicon.ico" type="image/x-icon" rel="shortcut icon" />
+
     <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="css/application.css">
-    
+
     <script type="text/javascript" charset="utf-8" src="js/jquery.min.js"></script>
-	<script type="text/javascript" charset="utf-8" src="js/bootstrap.min.js"></script>
+	  <script type="text/javascript" charset="utf-8" src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 
   </head>
   <body>
 
   <div class="container">
-    
+
     <?= $content ?>
-    
-  </div>  
+
+  </div>
 
   </body>
 </html>
